5990b38b5724044a2132e12e7f9655d45d735249
Anton Lofgren <alofgren@op5.com>
2014-09-02 11:12:56 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2
+#coding=utf-8
+from setuptools import setup, find_packages
+from subprocess import check_output
+
+def get_version():
+	tag = check_output(["git", "tag"])
+	hash = check_output(["git", "log", "-1", "--format=%H"])[:10]
+	return "%s-p%s" % (tag, hash)
+
+setup(
+		name = "gerrit-notify",
+		version = get_version(),
+		packages = find_packages(),
+		author = "Anton Löfgren",
+		author_email = "anton.lofgren@gmail.com",
+		license = "BSD (2-clause)",
+		keywords = "gerrit review notify notification daemon",
+		install_requires = open('requirements.txt').read().split('\n'),
+		entry_points = {
+			'console_scripts': [
+				'gerrit-notify = gerrit_notify.gerrit_notify:main',
+				],
+			}
+		)
